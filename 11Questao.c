4f5f0c4e86912a738641e36618ff7179c40ebdfd
Renato-Barreto <143633174+Renato-Barreto@users.noreply.github.com>
2023-11-07 16:35:08 -0300

    Add files via upload

---
+++ 11Questao.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <time.h>
+
+int main () {
+    setlocale(LC_ALL,"portuguese");
+
+    //Definindo Variáveis
+    char codigoEscrito [250];
+    char codigoSalvo [250] = "C4F3";
+
+    //Solicitando Dados
+    do {
+        fflush (stdin);
+        printf("Código de acesso: \n");
+        gets(codigoEscrito);
+
+        system("cls || clear");
+
+        if (strcmp(codigoEscrito, codigoSalvo) == 0) {
+            printf("Acesso permitido!");
+        } else {
+            printf("Código inválido! Digite novamente. \n");
+            sleep(3);
+            system("cls || clear");
+        }
+
+    } while (strcmp(codigoEscrito, codigoSalvo) !=0);
+    
+    return 0; 
+    
+    }
